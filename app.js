071b56e195f15f40172d3b7ceb2f80537b0b3bcc
LVC <levancu976@gmail.com>
2025-08-29 10:53:16 +0700

    update code app

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Docker!');
-    res.send('Hello from Docker! 2');
+    res.send('Hello from Docker! Hello from Docker! 2');
 });
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
